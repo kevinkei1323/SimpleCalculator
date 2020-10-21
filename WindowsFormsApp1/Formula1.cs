dd5f3abc2f76ed373e96df1ae892fa8d51e695ee
Kevin Datingaling <datingaling.014829@cubao.sti.edu.ph>
2020-10-21 20:37:54 +0800

    Initial Commit

---
+++ WindowsFormsApp1/Formula1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+
+    public delegate int GetAnswer(int x, int y);
+
+    class Formula1
+    {
+
+        public static int getSum(int num1, int num2)    //Button1
+        {
+
+            return num1 + num2;
+        }
+
+        public static int getSub(int num1, int num2)    //Button2
+        {
+
+            return num1 - num2;
+        }
+
+        public static int getMul(int num1, int num2)    //Button3
+        {
+
+            return num1 * num2;
+        }
+
+        public static int getDiv(int num1, int num2)    //Button4
+        {
+
+            return num1 / num2;
+        }
+    }
+}
